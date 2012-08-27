a22efb8a8e8ae38976c5d1019b9cd1477b3121d2
sragneau <sragneau@S1101215.angers.cnp.fr>
2012-08-27 17:04:01 +0200

    Ajout du bloc détail de l'anomalie

--- src/MyApp/GdaBundle/Controller/AnomalieController.php
+++ src/MyApp/GdaBundle/Controller/AnomalieController.php
@@ -1,4 +1,12 @@
 <?php
+/* HEADER
+ * 24/08/2012
+ * CREATION DU FICHIER 
+ * --------------------------
+ * 27/08/2012
+ * Ajout des méthodes modifier et supprimer 
+ */
+
 
 namespace MyApp\GdaBundle\Controller;
 
@@ -10,20 +18,58 @@ use MyApp\GdaBundle\Entity\Anomalie;
 use MyApp\GdaBundle\Form\AnomalieForm;
 
 class AnomalieController extends Controller {
+    
     /*
      *    Lister les anomalies non terminées
      *    Utilisation de QueryBuilder Object
      * 	  http://docs.doctrine-project.org/projects/doctrine-orm/en/2.1/reference/query-builder.html
      */
-
-    public function listerAction() {
+    public function listerAction($id) {
+        //TODO : Ajouter la requête avec le filtre de l'état
         $repository = $this->getDoctrine()->getRepository('MyAppGdaBundle:Anomalie');
         $anomalies = $repository->findAll();
         //Requête pour récupérer les anomalies non terminées
         /* $query = $repository->createQueryBuilder('a');
           $query->getSQL("select ");
           $anomalies= $query->getResult(); */
+        
+        // On remplit la zone de détail avec les éléments de l'anomalie sélectionnée 
+        if (isset($id)) 
+	{
+           // modification d'une anomalie existante
+            $anomalie = $em->find('MyAppGdaBundle:Anomalie', $id);
+            if (!$anomalie)
+            {
+                $message='Aucune anomalie trouvée';
+            }        
+            $form = $this->container->get('form.factory')->create(new DetailAnomalieForm(), $anomalie);
+        }
+        
+        
+	$request = $this->container->get('request');
 
+	if ($request->getMethod() == 'POST') 
+	{
+    	  	$form->bindRequest($request);
+
+            if ($form->isValid()) 
+            {
+                    $em->persist($anomalie);
+                    $em->flush();
+                    if (isset($id)) 
+                    {
+                            $message='Anomalie modifiée avec succès !';
+                    }
+                    else 
+                    {
+                            $message='Anomalie ajoutée avec succès !';
+                    }
+            }
+	}    
+        
+        
+        
+        
         return $this->container->get('templating')->renderResponse('MyAppGdaBundle:Anomalie:lister.html.twig', array(
                     'anomalies' => $anomalies
                 ));
@@ -32,8 +78,8 @@ class AnomalieController extends Controller {
     /*
      * Créer une anomalie     
      */
-
-    public function creerAction() {
+     // TODO à supprimer le bloc creer
+    /*public function creerAction() {
         $message = '';
         $anomalie = new Anomalie();
         $form = $this->container->get('form.factory')->create(new AnomalieForm(), $anomalie);
@@ -56,16 +102,80 @@ class AnomalieController extends Controller {
                     'form' => $form->createView(),
                     'message' => $message,
                 ));
+    }*/
+
+    public function anomalieAction($id) {       
+        
+        $message='';
+	$em = $this->container->get('doctrine')->getEntityManager();
+
+	if (isset($id)) 
+	{
+		// modification d'une anomalie existante
+		$anomalie = $em->find('MyAppGdaBundle:Anomalie', $id);
+
+		if (!$anomalie)
+		{
+                    $message='Aucune anomalie trouvée';
+		}
+	}
+	else 
+	{
+		// ajout d'une nouvelle anomalie
+		$anomalie = new Anomalie();
+	}
+
+	$form = $this->container->get('form.factory')->create(new AnomalieForm(), $anomalie);
+
+	$request = $this->container->get('request');
+
+	if ($request->getMethod() == 'POST') 
+	{
+    	  	$form->bindRequest($request);
+
+            if ($form->isValid()) 
+            {
+                    $em->persist($anomalie);
+                    $em->flush();
+                    if (isset($id)) 
+                    {
+                            $message='Anomalie modifiée avec succès !';
+                    }
+                    else 
+                    {
+                            $message='Anomalie ajoutée avec succès !';
+                    }
+            }
+	}
+
+	return $this->container->get('templating')->renderResponse('MyAppGdaBundle:Anomalie:creer.html.twig',
+	array(
+	'form' => $form->createView(),
+	'message' => $message,
+	));
+                    
     }
 
-    public function modifierAction($id) {
-        return $this->container->get('templating')->renderResponse(
-                        'MyAppGdaBundle:Anomalie:modifier.html.twig');
-    }
-
+    
+    /*
+     * Supprimer une anomalie     
+     */
     public function supprimerAction($id) {
-        return $this->container->get('templating')->renderResponse(
-                        'MyAppGdaBundle:Anomalie:supprimer.html.twig');
+    
+        $em = $this->container->get('doctrine')->getEntityManager();
+        $anomalie = $em->find('MyAppGdaBundle:Anomalie', $id);
+
+        if (!$anomalie) 
+        {
+          throw new NotFoundHttpException("Anomalie non trouvée");
+        }
+
+        $em->remove($anomalie);
+        $em->flush();        
+
+
+        return new RedirectResponse($this->container->get('router')->generate('myapp_anomalie_lister'));
+          
     }
 
 }
